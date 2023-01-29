1c45a3350503428b82d17066d78c20a2951a5e14
James Camp <jcamp@tamu.edu>
2023-01-29 12:36:37 -0600

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,38 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  sum = 0
+  arr.each{|num| sum += num}
+  return sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  sorted = arr.sort
+  if sorted.empty? then
+    result = 0
+  elsif sorted.size == 1 then
+    result = sorted[0]
+  else
+    result = (sorted[-1]+sorted[-2])
+  end
+  return result
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  if arr.empty? then
+    return false
+  elsif arr.size == 1
+    return false
+  else
+    for i in 0...(arr.size-1) do
+      for j in (i+1)...arr.size do
+        if arr[i] + arr[j] == number
+          return true
+        end
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
